c002f0afe417cc02c72037bd45a4b6e09fb96bdb
Cyan <user@email>
2025-11-11 16:28:03 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from logger import log_state
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    '''
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    '''
+    pygame.init()
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #feels cursed to do this, but fine.
+    while True:
+        #logging state
+        log_state()
+        
+        #glad we have this now
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        #refresh screen
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
